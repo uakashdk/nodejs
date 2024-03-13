60da8761bd59010598e5f111d8d02d038fa62de1
Akash Dubey <uakashdk@gmail.com>
2024-03-13 18:22:34 +0530

    message

---
+++ index.js
@@ -0,0 +1,6 @@
+const express=require("express")
+var color=require("colors")
+const chalk=require("chalk")
+console.log('hello'.green);
+console.log('Namste'.blue);
+console.log(chalk.blue('This text will be blue'));
